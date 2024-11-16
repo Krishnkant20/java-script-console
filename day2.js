81a89895daa9a063ac0dddc9fda639b1cc58ce12
Krishnkant20 <krishnakantxr@gmail.com>
2024-11-16 20:15:21 +0530

    updated

---
+++ day2.js
@@ -0,0 +1,32 @@
+// var let const
+
+var num=25;
+
+// run java script
+
+function run(){
+        // var num=45;
+        // console.log(num)
+        // let num1=55;
+        // console.log(num1)
+        // const num2=60;
+        // console.log(num2)
+        //scope============================
+        // const num=55;
+        // {
+        //     const num=10;
+        //     console.log(num)
+        // }
+        // console.log(num)
+
+        //======================== redeclaration
+        // const num=44;
+        // const num=55;
+        // console.log(num)
+
+        //reassingment============================
+        // const num=45;
+        // num =34;
+        // console.log(num)
+}
+run();
