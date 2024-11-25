782707423181f082db3bc2254ec70f668769406b
Krishnkant20 <krishnakantxr@gmail.com>
2024-11-25 19:50:34 +0530

    updated

--- day2.js
+++ day2.js
@@ -1,106 +1,126 @@
-// var let const
+// // var let const
 
-// var num=25;
+// // var num=25;
 
-// run java script
+// // run java script
 
+// // function run(){
+//         // var num=45;
+//         // console.log(num)
+//         // let num1=55;
+//         // console.log(num1)
+//         // const num2=60;
+//         // console.log(num2)
+//         //scope============================
+//         // const num=55;
+//         // {
+//         //     const num=10;
+//         //     console.log(num)
+//         // }
+//         // console.log(num)
+
+//         //======================== redeclaration
+//         // const num=44;
+//         // const num=55;
+//         // console.log(num)
+
+//         //reassingment============================
+//         // const pi=3.14;
+//         // const num=45;
+//         // num =34;
+//         // console.log(num)
+// // }
+// // run();
+
+// // function oper(){
+// // alert("Hello i am alert message")
+// // prompt("Hello i am prompt message")
+// // confirm("Hello confirmed")
+
+// // let num1=2;
+// // let num2=3;
+// // console.log(num1+num2)
+// // alert( "the sum is" +(num1+num2))
+// //  let num1= parseInt(prompt("enter first number"));
+// //  let num2= parseInt(prompt("enter second number"));
+// // alert(num1+num2);
+// // let num1= prompt("enter first number");
+// // let num2= prompt("enter second number");
+// // alert(num1-num2);
+// // }
+// // oper();
+// // function run(){
+// //         alert("hello")
+// // }
+// // function run(){
+// //         prompt("hello")
+// // }
+// // function run(){
+// //         prompt("hello")
+// // }
 // function run(){
-        // var num=45;
-        // console.log(num)
-        // let num1=55;
-        // console.log(num1)
-        // const num2=60;
-        // console.log(num2)
-        //scope============================
-        // const num=55;
-        // {
-        //     const num=10;
-        //     console.log(num)
-        // }
-        // console.log(num)
-
-        //======================== redeclaration
-        // const num=44;
-        // const num=55;
-        // console.log(num)
-
-        //reassingment============================
-        // const pi=3.14;
-        // const num=45;
-        // num =34;
-        // console.log(num)
-// }
-// run();
-
-// function oper(){
-// alert("Hello i am alert message")
-// prompt("Hello i am prompt message")
-// confirm("Hello confirmed")
-
-// let num1=2;
-// let num2=3;
-// console.log(num1+num2)
-// alert( "the sum is" +(num1+num2))
-//  let num1= parseInt(prompt("enter first number"));
-//  let num2= parseInt(prompt("enter second number"));
-// alert(num1+num2);
-// let num1= prompt("enter first number");
-// let num2= prompt("enter second number");
-// alert(num1-num2);
-// }
-// oper();
-// function run(){
-//         alert("hello")
-// }
-// function run(){
-//         prompt("hello")
-// }
-// function run(){
-//         prompt("hello")
-// }
-function run(){
-// // let num= prompt("enter your age");
-// // num>18?alert("uh can vote"):alert("uh cant vote")
-// let num1=confirm("you have complete the course");
-// let num2=confirm("you have certificate");
-// if(num1&&num2){
-//         alert("completed")
-// }
-// else{
-//         alert("not completed")
-// }
-// let num1=prompt("check the number even or odd");
-// if(num1%2==0){
-//         alert("even number");
-// }
-// else{
-//         alert("odd number");
-// } 
-//  let num=confirm("are you sure to delete");
-// if(num){
-//         alert("item delete")
-// }else{
-//         alert("cancelled")
+// // // let num= prompt("enter your age");
+// // // num>18?alert("uh can vote"):alert("uh cant vote")
+// // let num1=confirm("you have complete the course");
+// // let num2=confirm("you have certificate");
+// // if(num1&&num2){
+// //         alert("completed")
+// // }
+// // else{
+// //         alert("not completed")
+// // }
+// // let num1=prompt("check the number even or odd");
+// // if(num1%2==0){
+// //         alert("even number");
+// // }
+// // else{
+// //         alert("odd number");
+// // } 
+// //  let num=confirm("are you sure to delete");
+// // if(num){
+// //         alert("item delete")
+// // }else{
+// //         alert("cancelled")
 
 
-// document.write("hello world")
-let para=document.querySelector("#dot");
-//  para.innerHTML="krishnkant ";
-//  para.style.color="purple"
-//  para.style.fontSize="80px"
-//  para.style.backgroundColor="yellow"
-// if(para.style.display==="none"){
-//         para.style.display="block";
-// } else{
-//         para.style.display="none";
+// // document.write("hello world")
+// let para=document.querySelector("#dot");
+// //  para.innerHTML="krishnkant ";
+// //  para.style.color="purple"
+// //  para.style.fontSize="80px"
+// //  para.style.backgroundColor="yellow"
+// // if(para.style.display==="none"){
+// //         para.style.display="block";
+// // } else{
+// //         para.style.display="none";
+// // }
+// // para.style.width="200px"
+// // para.style.height="300px"
+// // para.style.backgroundColor="yellow"
+// // para.style.onmousEenter="green"
+// // para.innerHTML= "<h1>hello student</h1>"
+// // para.style.border=("1px solid red")
+// // para.style.backgroundColor=("green") 
+
+
+
+
+
 // }
-para.style.width="200px"
-para.style.height="300px"
-para.style.backgroundColor="yellow" 
-
-
+let count=0;
+function inc(){
+count++;
+let para=document.querySelector("#text");
+para.innerHTML=count; 
 
 
 }
+function dec(){
+        count--;
+        let para=document.querySelector("#text");
+        para.innerHTML=count; 
+        
+        
+        }
 
  
