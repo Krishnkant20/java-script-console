1ad86da9f967a49b241d77ac1fa4f535cb505745
Krishnkant20 <krishnakantxr@gmail.com>
2024-12-14 11:14:27 +0530

    updated calculator

---
+++ calculator.js
@@ -0,0 +1,26 @@
+let store='';
+
+let cal=(value)=>{
+    let show=document.querySelector("#show")
+    store=store+value //"2+5"
+    show.innerHTML=store
+}
+
+let result=()=>{
+    let show=document.querySelector("#show")
+
+    show.innerHTML=eval(store) //"2+5"=>7 //number
+    store=eval(store) //7 number
+    store.toString(); //"7" string
+}
+let ac=()=>{
+    store="";
+    let show=document.querySelector("#show")
+    show.innerHTML=""
+}
+let del=()=>{
+     store=store.slice(0,-1)
+
+     let show=document.querySelector("#show")
+     show.innerHTML=store;
+}
--- object.js
+++ object.js
@@ -32,28 +32,33 @@
 //--------------------- destructuring-------------------
 
  const st=()=>{
-    // let student={name:"sahil",age:23,city:"bhopal"}
-    // const {name,age,city}=student;
-    // console.log(age)
+   //  let student={name:"sahil",age:23,city:"bhopal"}
+   //  const {name,age,city}=student;
+   //  console.log(age)
 
 
     // spread operator(...)-------------
      
-    // let student1={name:"sahil",age:23,city:"bhopal"}
-    // let student2={address:"babai",mobile:1234567890}
+   //  let student1={name:"sahil",age:23,city:"bhopal"}
+   //  let student2={address:"babai",mobile:1234567890}
     
     // let newstudent={...student1,...student2}
     // console.log(newstudent);
 
     //calculation---------------
 
-    let arr1=[1,2,3,4,5,6]
+   //  let arr1=[1,2,3,4,5,6]
     // let newarr=arr1.map((num)=>num*2)  //calculate by 2
 
     // let newarr=arr1.filter((num)=>num>3)  // number greater than by three
 
-    console.log(newarr);
-
+    // let arr1=[1,2,3,4,5]
+    // let newarr=arr1.map((num)=>num*2)
+    // console.log(newarr)
+ 
+    // let arr2=[3,5,7,6,8,9]
+    // let farr=arr2.filter((num)=>num>3)
+    // console.log(farr)
 
  }
  st()
