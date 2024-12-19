e04069ce718e5e4537756bf15fe3a8c61cfd8d63
Krishnkant20 <krishnakantxr@gmail.com>
2024-12-19 19:56:40 +0530

    updated

---
+++ slider.js
@@ -0,0 +1,12 @@
+ let swiper= new Swiper('.swiper',{
+
+
+    // direction: 'vertical',
+    // loop: true,
+
+    navigation: {
+        nextEl: '.swipernext',
+        prevEl: '.swipernext2',
+
+      },
+ });
